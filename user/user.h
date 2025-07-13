fb4667d45d0d03c6e593849403c1de556a19b074
parsa <43097056+parsashahkar21@users.noreply.github.com>
2025-07-14 03:12:35 +0330

    change in user files

--- user/user.h
+++ user/user.h
@@ -22,6 +22,8 @@ int getpid(void);
 char* sbrk(int);
 int sleep(int);
 int uptime(void);
+int thread(void *start_thread, int *stack_address, void *arg);
+int jointhread(int id);
 
 // ulib.c
 int stat(const char*, struct stat*);
